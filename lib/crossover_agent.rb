fb69c7c2aedb4aafd108e502562d369396c06bae
Truong Hoang Dung <checkraiser11@gmail.com>
2016-04-30 16:33:29 +0700

    Fix args

--- lib/crossover_agent.rb
+++ lib/crossover_agent.rb
@@ -32,8 +32,8 @@ module CrossoverAgent
         cfg.server = options['-s'] if options['-s']
         cfg.port = options['-p'] if options['-p']
         cfg.auth_token = options['-t'] if options['-t']
-        cfg.limit = options['-l'] if options['-l']
-        cfg.delay = options['-d'] if options['-d']
+        cfg.limit = options['-l'].to_i if options['-l']
+        cfg.delay = options['-d'].to_i if options['-d']
       end
       agent.execute
     end
